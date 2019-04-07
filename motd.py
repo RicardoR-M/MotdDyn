3589f3f60504bd87538153fb1f0cc9bb83a07c81
Complex <Complex@EDEN>
2019-04-07 16:55:42 -0500

    Se implementa el uso de mutex para que solo una instancia del programa se ejecute en todo momento, aun cuando se lance más de una vez.

--- motd.py
+++ motd.py
@@ -1,8 +1,27 @@
 import sys
 
 from PyQt5 import QtWidgets, QtCore, QtGui
+from win32api import CloseHandle, GetLastError
+from win32event import CreateMutex
+from winerror import ERROR_ALREADY_EXISTS
 
 from UI.UI_motd import Ui_MainWindow
+
+
+class SingleInstance:
+    """ Limits application to single instance"""
+
+    def __init__(self):
+        self.mutexname = "DynMotd_{D0E858DF-985E-4907-B7FB-6D732C3FC3B6}"
+        self.mutex = CreateMutex(None, False, self.mutexname)
+        self.lasterror = GetLastError()
+
+    def alreadyrunning(self):
+        return self.lasterror == ERROR_ALREADY_EXISTS
+
+    def __del__(self):
+        if self.mutex:
+            CloseHandle(self.mutex)
 
 
 class MainW(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -27,7 +46,7 @@ class MainW(QtWidgets.QMainWindow, Ui_MainWindow):
     def salir(self):
         self.flag_salir = True
         # print("salir")
-        sys.exit()
+        sys.exit(0)
 
     def closeEvent(self, event):
         if not self.flag_salir:
@@ -52,7 +71,7 @@ class MainW(QtWidgets.QMainWindow, Ui_MainWindow):
         if self.geometry().x() > x - self.width() + 80:
             self.move(self.geometry().x() - self.width(), self.geometry().y())
 
-        if self.geometry().y() < -(self.height()*0.45):
+        if self.geometry().y() < -(self.height() * 0.45):
             self.move(self.geometry().x(), self.geometry().y() + self.height())
         if self.geometry().y() > y - self.height() + (self.height() * 0.45):
             self.move(self.geometry().x(), self.geometry().y() - self.height())
@@ -62,6 +81,14 @@ class MainW(QtWidgets.QMainWindow, Ui_MainWindow):
 
 
 if __name__ == '__main__':
+    # instancia mutex
+    me = SingleInstance()
+
+    # check if another instance of some program running
+    if me.alreadyrunning():
+        # print("Another instance running")
+        sys.exit(-1)
+
     app = QtWidgets.QApplication(sys.argv)
     myapp = MainW()
     myapp.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.Tool)
